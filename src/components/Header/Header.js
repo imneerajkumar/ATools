43e6640cf392f9a1190a4d46114d457d59b912dd
Neeraj Kumar <nijjukr@gmail.com>
2022-02-08 17:42:07 +0530

    App initialized

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css';
+
+function Header(props) {
+  return (
+    <div className='Header'>
+      This is Header
+    </div>
+  );
+}
+
+export default Header;
---
+++ src/components/Login/Login.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Login.css';
+
+function Login(props) {
+  return (
+    <div>
+      This is login.
+    </div>
+  );
+}
+
+export default Login;
